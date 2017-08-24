deb5b4bb72de4e88c8c9762191bd59f8dba00ae9
unknown <>
2017-08-24 23:37:36 +0800

    share

--- React/豆瓣电影/my-app/src/components/diff/index-left-top.js
+++ React/豆瓣电影/my-app/src/components/diff/index-left-top.js
@@ -11,7 +11,8 @@ export default class IndexLeftTop extends Component{
     constructor(){
         super();
         this.state={
-            hotData:''
+            hotData:'',
+            model:'next'
         }
     }
     componentDidMount(){
@@ -26,11 +27,108 @@ export default class IndexLeftTop extends Component{
             }
         });
     }
+    starShow=(data)=>{
+        let Data = data.rating;
+        const total = 5;
+        let full,half,empty;
+        let arr = [];
+        if(Data.average*1){
+            full = Math.floor(Data.stars/10);
+            half = Math.ceil(Data.stars/10) - full;
+            empty = total - full - half;
+            for(let i = 0;i<full;i++){
+                arr.push(
+                    <img 
+                    className="fl"
+                    key={'full'+i}
+                    src={require('../../images/left/full-star.png')} />
+                ); 
+            }
+            if(half){
+                for(let i = 0;i<half;i++){
+                    arr.push(
+                        <img 
+                        className="fl"
+                        key={'half'+i}
+                        src={require('../../images/left/half-star.png')} />
+                    ); 
+                }
+            }
+            if(empty){
+                for(let i = 0;i<empty;i++){
+                    arr.push(
+                        <img 
+                        className="fl"
+                        key={'empty'+i}
+                        src={require('../../images/left/empty-star.png')} />
+                    ); 
+                } 
+            }
+            arr.push(
+                <span 
+                key={'span'}
+                className="fl">
+                    {Data.average}
+                </span>
+            )
+            return arr;
+        }else{
+            return(
+              '暂无评分'  
+            );
+        }
+        
+    }
     imgShow=(data,num,model)=>{
+        let start,end;
+        model = this.state.model;
+        if(model === 'next'){
+            start = data.slice((num-1)*4,num*4);
+            end = data.slice(num*4,(num+1)*4)?data.slice(num*4,(num+1)*4):data.slice(0,4);
+        }else if(model === 'prev'){
+            start = data.slice((num-1)*4,num*4);
+            end = data.slice((num-2)*4,(num-1)*4)?data.slice((num-2)*4,(num-1)*4):data.slice(4,5);
+        }
         return(
-            <ul>
-                <li></li>
-                <li></li>
+            <ul className="clearfix hot-img-show">
+                <li 
+                key={'li1'}
+                className="fl">
+                    {
+                        start.map((e,i)=>{
+                            return (
+                                <div 
+                                key={e.id+Math.random()}
+                                className="fl">
+                                    <dl>
+                                        <dt>
+                                            <Link to={`/subject/${e.id}`}>
+                                                <img 
+                                                className="hot-img-show-dt"
+                                                alt={e.alt}
+                                                src={e.images.large} />
+                                            </Link>
+                                        </dt>
+                                        <dd key={'dd1'}>
+                                            <Link to={`/subject/${e.id}`}>
+                                                {e.title}
+                                            </Link>
+                                        </dd>
+                                        <dd 
+                                        className="clearfix"
+                                        key={'dd2'}>
+                                            {this.starShow(e)}
+                                        </dd>
+                                        <dd key={'dd3'}></dd>
+                                    </dl>
+                                </div>
+                            )
+                        })
+                    }
+                </li>
+                <li 
+                key={'li2'}
+                className="fl"></li>
             </ul>
         );
     }
@@ -56,8 +154,8 @@ export default class IndexLeftTop extends Component{
                         {num+'/'+data.length/4}
                     </span>
                 </h2>
-                <div className="hot-img-show">
-
+                <div className="hot-img-show-box">
+                    {this.imgShow(data,num)}
                 </div>
             </div>
         );
